b8c8fb93beecf05166d808d9a391946f8921a9fd
iylee <iylee@markany.com>
2021-09-16 18:14:41 +0900

    db 연결 오류 수정

--- server.js
+++ server.js
@@ -3,29 +3,40 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-app.use(cors());
 const port = 5001;
+
+app.use(cors());
+
 var data_cout = 0;
 
-const { Client } = require('pg');
+//const { Client } = require('pg');
+const { Pool } = require('pg');
 
-var client = null
+//var client = null
+var pool = null
 
 app.get('/count', (req, res) => {
+  try{
 
-  client.connect();
+    pool.query('SELECT count(*) as ret from events', (err, result) => {
+      try{
+        console.log("ret:"+ result.rows[0].ret)
+        data_cout = result.rows[0].ret
 
-  client.query('SELECT count(*) as ret from events', (err, res) => {
-      console.log("ret:"+ res.rows[0].ret)
-      data_cout = res.rows[0].ret
-      //console.log(err, res)
-      client.end()
+        //res.send('data_cout :'  + data_cout)
 
-      
-  });  
+        res.json({
+          data_cout: data_cout,
+        });
 
-  res.send('data_cout :'  + data_cout)
- 
+      }catch(e){
+        console.log("[COUNT] res error:" + e);  //e.stack
+      }      
+        
+    });  
+  }catch(e){
+    console.log("[COUNT] error:" + e);
+  }  
 })
 
 app.get('/json', function(req, res) {
@@ -37,37 +48,71 @@ app.get('/json', function(req, res) {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`app listening at http://localhost:${port}`)
   init()
 })
 
 function init() {
 
-  var db_host = 'localhost';
-  var db_user = 'pdb';
-  var db_database = 'log_db';
-  var db_password = '4Z2CrYcasdfLe0OUM9YD';
+  try{
+    var db_host = 'localhost';
+    var db_port = 5432;
+    var db_user = 'pdb';
+    var db_database = 'log_db';
+    var db_password = '4Z2CrYcasdfLe0OUM9YD';
 
-  if (process.env.LOG_DB_ADDRESS != null && process.env.LOG_DB_ADDRESS.length > 0) {
-    db_host = process.env.LOG_DB_ADDRESS;
+    //process.env.LOG_DB_ADDRESS = "localhost:5432";
+
+    if (process.env.LOG_DB_ADDRESS != null && process.env.LOG_DB_ADDRESS.length > 0) {      
+      db_host = process.env.LOG_DB_ADDRESS;
+      if (db_host.indexOf(":") != -1) {
+        var host = db_host.split(":")[0];
+        var port = db_host.split(":")[1];
+
+        db_host = host;
+        db_port = Number(port);
+      }
+    }
+    if (process.env.LOG_DB_PASSWORD != null && process.env.LOG_DB_PASSWORD.length > 0) {
+      db_password = process.env.LOG_DB_PASSWORD;
+    }  
+
+    console.log("==============================");
+    console.log(`[INIT] db_host:${db_host}`);
+    console.log(`[INIT] db_port:${db_port}`);
+    console.log(`[INIT] db_user:${db_user}`);
+    console.log(`[INIT] db_database:${db_database}`);
+    //console.log(`db_password:${db_password}`);
+
+    pool = new Pool({    
+      host : db_host,
+      database : db_database,
+      user : db_user,
+      password : db_password,
+      port : db_port,
+      max: 5,
+      min: 2,
+      idleTimeoutMillis: 30000,
+      connectionTimeoutMillis: 2000,      
+    });
+
+    pool.on("error", (err) => console.error('[POOL] idle client error:', err));  //important for db restart - 2021.09.15 IYLEE
+
+    // callback - checkout a client
+    pool.connect((err, client, done) => {
+      if (err) throw err;
+      client.query('SELECT NOW()', [], (err, res) => {
+        done()
+        if (err) {
+          console.log(err.stack);
+        } else {
+          console.log("[INIT] db connect ok!");
+          console.log(res.rows[0]);
+        }
+      })
+    })    
+
+  }catch(e){
+    console.log("[INIT] error:" + e);
   }
-  if (process.env.LOG_DB_PASSWORD != null && process.env.LOG_DB_PASSWORD.length > 0) {
-    db_password = process.env.LOG_DB_PASSWORD;
-  }  
-
-  console.log("==============================");
-  console.log(`db_host:${db_host}`);
-  console.log(`db_user:${db_user}`);
-  console.log(`db_database:${db_database}`);
-  console.log(`db_password:${db_password}`);
-
-  client = new Client({    
-    host : db_host,
-    database : db_database,
-    user : db_user,
-    password : db_password,
-    port : 5432,
-  });
-
-  client.connect();
 }
